7bad54deb845616a31b35eab10189d3a346fcf03
Starry Nimbus <starrynimbus01000@gmail.com>
2026-02-15 22:42:21 -0800

    Fix app launch issue

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
 
   QQmlApplicationEngine engine;
   engine.addImportPath("qrc:/");
-  engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
+  engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
   if (engine.rootObjects().isEmpty()) {
     return 1;
   }
